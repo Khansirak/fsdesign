10beb6f419159a48edbeafd5e5f7a09f8fad4560
Khansirak <khan.sirak@bakerhicks.at>
2023-05-30 10:12:09 +0200

    Updates

--- text.js
+++
@@ -1,7 +0,0 @@
-// Add this in node_modules/react-dom/index.js
-Window.React1 = require('react');
-
-// Add this in your component file
-require('react-dom');
-window.React2 = require('react');
-console.log(window.React1 === window.React2);
